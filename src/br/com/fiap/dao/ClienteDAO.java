d00290c9a02dc718e91c8675407a55deaebdf875
Guilherme de Araujo <guiam2909@gmail.com>
2026-09-04 08:39:28 -0300

    feat: implementando Cliente no banco de dados

---
+++ src/br/com/fiap/dao/ClienteDAO.java
@@ -0,0 +1,11 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.dto.Cliente;
+
+import java.sql.Connection;
+
+public class ClienteDAO {
+    private Connection con;
+
+
+}
---
+++ src/br/com/fiap/dto/Cliente.java
@@ -0,0 +1,7 @@
+package br.com.fiap.dto;
+
+public class Cliente {
+    private int idCliente;
+    private String nomeCliente;
+    private String placa;
+}
