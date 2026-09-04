e33e7e777abeb3fb7e4301dfbeb22046c9a214c1
Guilherme de Araujo <guiam2909@gmail.com>
2026-09-04 09:00:30 -0300

    feat: classes dto, dao e crud finalizados

--- src/br/com/fiap/dto/Cliente.java
+++ src/br/com/fiap/dto/Cliente.java
@@ -4,4 +4,28 @@ public class Cliente {
     private int idCliente;
     private String nomeCliente;
     private String placa;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
 }
